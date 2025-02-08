3b06550b218a70e630eff15b7eb36c37893a8bb9
Norman Sobczyk <wgwcompany@duck.com>
2025-02-08 15:55:27 +0000

    update

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,5 +1,12 @@
 import { getRequestContext } from '@cloudflare/next-on-pages';
 
+async function hashPassword(password) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+};
 
 export async function POST(request) {
   const requestBody = await request.text();
@@ -10,23 +17,28 @@ export async function POST(request) {
       status: 400,
       headers: { 'Content-Type': 'application/json' }
     });
-  };
+  }
 
   try {
-    const db = getRequestContext().env.DATABASE;;
+    const hashedPassword = await hashPassword(password);
+    const db = getRequestContext().env.DATABASE;
 
     const result = await db.prepare(`
       INSERT INTO users (username, email, password)
       VALUES (?, ?, ?)
-    `).bind(username, email, password).run();
-
+    `).bind(username, email, hashedPassword).run();
 
     return new Response(JSON.stringify({ message: 'User registered successfully', userId: result.lastInsertRowid }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
-    console.error('Database error:', error);
+    if (error.message.includes('UNIQUE constraint failed')) {
+      return new Response(JSON.stringify({ error: 'Email already exists' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
 
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
