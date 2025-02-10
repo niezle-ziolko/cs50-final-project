4ea313df0a39b690931b1881485266f096ff38a2
Norman Sobczyk <wgwcompany@duck.com>
2025-02-10 16:55:31 +0000

    update

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,13 +1,14 @@
 'use client';
 import { useState } from 'react';
 import Script from 'next/script';
-import { useRouter } from 'next/navigation';
-import { signIn } from 'next-auth/react';
+
+import { useSession } from 'context/user-context';
 
 import 'styles/css/theme/forms.css';
 
+
 export default function Login() {
-  const router = useRouter();
+  const { saveSession } = useSession();
   const [errorMessage, setErrorMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -22,7 +23,6 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrorMessage('');
-    setLoading(true);
 
     const formState = new FormData(e.target);
     const turnstileRes = formState.get('cf-turnstile-response');
@@ -56,19 +56,30 @@ export default function Login() {
       });
 
       if (response.ok) {
-
-        const result = await signIn('credentials', {
-          redirect: false,
-          username: formData.username,
-          password: formData.password
+        setLoading(true);
+        const credentials = Buffer.from(`${formData.username}:${formData.password}`).toString('base64');
+        
+        const res = await fetch('/api/auth/user', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_CLIENT_AUTH}`,
+            'Credentials': credentials
+          }
         });
 
+        const data = await res.json();
+        console.log(data);
         setLoading(false);
 
-        if (result?.error) {
-          setErrorMessage('Invalid credentials');
+        if (res.ok) {
+          const setCookieHeader = res.headers.get('Set-Cookie');
+
+          if (setCookieHeader) {
+            saveSession(setCookieHeader);
+          };
         } else {
-          router.push('/auth/my-account');
+          setErrorMessage(`Error: ${data.error}`);
         };
       };
     } catch (error) {
@@ -89,8 +100,8 @@ export default function Login() {
           <input className='input' name='username' placeholder='Username' type='text' value={formData.username} onChange={handleChange} required />
           <input className='input' name='password' placeholder='Password' type='password' value={formData.password} onChange={handleChange} required />
           <div className='cf-turnstile' data-sitekey={TURNSTILE_SITE_KEY} data-callback='javascriptCallback' data-theme='dark' />
-          {errorMessage && <p className='error-message'>{errorMessage}</p>}
-          <button className='btn' type='submit' disabled={loading}>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <button className="btn" type="submit" disabled={loading}>
             {loading ? 'Submitting...' : 'Submit'}
           </button>
         </form>
