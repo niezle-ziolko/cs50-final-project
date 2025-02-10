f5270c0c4fdb2cd82b45d8e16e015b6db7c5cc18
Norman Sobczyk <wgwcompany@duck.com>
2025-02-10 16:37:31 +0000

    update

--- src/app/auth/my-account/page.jsx
+++ src/app/auth/my-account/page.jsx
@@ -4,10 +4,6 @@ import { useSession } from 'context/user-context';
 export default function MyAccount() {
   const { user } = useSession();
 
-  if (typeof user === 'undefined') {
-    return <div>Ładowanie...</div>;
-  }
-
   return <div>{user?.id ?? 'Brak ID użytkownika'}</div>;
 }
 
