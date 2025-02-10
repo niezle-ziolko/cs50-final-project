91e92c0f71b254587565a69ca4adffb28a1f4e02
Norman Sobczyk <wgwcompany@duck.com>
2025-02-10 15:03:51 +0000

    update

--- src/app/auth/my-account/page.jsx
+++ src/app/auth/my-account/page.jsx
@@ -1,22 +1,14 @@
 'use client';
 import { useSession } from 'context/user-context';
-import { useEffect, useState } from 'react';
 
 export default function MyAccount() {
   const { user } = useSession();
-  const [loadedUser, setLoadedUser] = useState(null);
 
-  useEffect(() => {
-    if (user) {
-      setLoadedUser(user);
-    }
-  }, [user]);
-
-  if (!loadedUser) {
+  if (typeof user === 'undefined') {
     return <div>Ładowanie...</div>;
   }
 
-  return <div>{loadedUser.id}</div>;
+  return <div>{user?.id ?? 'Brak ID użytkownika'}</div>;
 }
 
 export const runtime = 'edge';
