d5036516f53992133d3852117a6f4de3c388c55e
_niezle_ziolko <wgwcompany@duck.com>
2025-02-16 09:13:13 +0100

    update

--- src/app/components/player.jsx
+++ src/app/components/player.jsx
@@ -8,41 +8,40 @@ export default function AudioPlayer() {
 
   return (
     <div className='player'>
-      {currentPicture && (
-        <div className='image'>
-          <img src={currentPicture} alt='Current Book Cover' />
-        </div>
-      )}
-      <media-controller audio>
-        <audio
-          slot='media'
-          src={currentFile}
-          crossOrigin='true'
-        />
-        <media-control-bar>
-          <div className='box'>
-            <media-time-display></media-time-display>
-            <media-time-range></media-time-range>
-            <media-duration-display></media-duration-display>
+      <div className='container'>
+        <p className='heading'>Currently book playing</p>
+        {currentPicture && (
+          <div className='image'>
+            <img src={currentPicture} alt='Currently book cover' />
           </div>
-          <div className='box'>
-            <media-seek-backward-button>
-              <i slot='icon' className='fa-solid fa-arrow-rotate-left'></i>
-            </media-seek-backward-button>
-            <media-play-button>
-              <i slot='play' className='fa-solid fa-play'></i>
-              <i slot='pause' className='fa-solid fa-pause'></i>
-            </media-play-button>
-            <media-seek-forward-button>
-              <i slot='icon' className='fa-solid fa-arrow-rotate-right'></i>
-            </media-seek-forward-button>
-            <media-mute-button>
-              <i slot='high' className='fa-solid fa-volume-high'></i>
-              <i slot='off' className='fa-solid fa-volume-xmark'></i>
-            </media-mute-button>
-          </div>
-        </media-control-bar>
-      </media-controller>
+        )}
+        <media-controller audio>
+          <audio slot='media' src={currentFile} crossOrigin='true' />
+          <media-control-bar>
+            <div className='box'>
+              <media-time-display></media-time-display>
+              <media-time-range></media-time-range>
+              <media-duration-display></media-duration-display>
+            </div>
+            <div className='box'>
+              <media-seek-backward-button>
+                <i slot='icon' className='fa-solid fa-arrow-rotate-left'></i>
+              </media-seek-backward-button>
+              <media-play-button>
+                <i slot='play' className='fa-solid fa-play'></i>
+                <i slot='pause' className='fa-solid fa-pause'></i>
+              </media-play-button>
+              <media-seek-forward-button>
+                <i slot='icon' className='fa-solid fa-arrow-rotate-right'></i>
+              </media-seek-forward-button>
+              <media-mute-button>
+                <i slot='high' className='fa-solid fa-volume-high'></i>
+                <i slot='off' className='fa-solid fa-volume-xmark'></i>
+              </media-mute-button>
+            </div>
+          </media-control-bar>
+        </media-controller>
+      </div>
     </div>
   );
 };
