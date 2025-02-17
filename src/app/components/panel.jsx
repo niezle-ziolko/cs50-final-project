383987bdc2598ac740b2c3ce83afaf310de5d3cc
Norman Sobczyk <wgwcompany@duck.com>
2025-02-17 14:10:14 +0000

    update

--- src/app/components/panel.jsx
+++ src/app/components/panel.jsx
@@ -86,13 +86,17 @@ export default function ClientPanel({ title }) {
               }}>
                 <td>
                   <img src={book.picture} alt={book.title} width='205' height='290' />
-                  <div className='background-icon'>
-                    {selectedBookId === book.id ? (
-                      <Playing />
-                    ) : (
-                      <i className='fa-solid fa-play' id='icon' />
-                    )}
-                  </div>
+                  {title === 'Library' ? (
+                    <div className='background-icon'>
+                      {selectedBookId === book.id ? (
+                        <Playing />
+                      ) : (
+                        <i className='fa-solid fa-play' id='icon' />
+                      )}
+                    </div>
+                  ) : (
+                    <></>
+                  )}
                 </td>
               </tr>
             ))
