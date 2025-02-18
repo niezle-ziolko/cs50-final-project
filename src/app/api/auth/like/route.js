8a3c253202c7ed11ac737610f99538f52e820aca
Norman Sobczyk <wgwcompany@duck.com>
2025-02-18 08:28:57 +0000

    update

--- src/app/api/auth/like/route.js
+++ src/app/api/auth/like/route.js
@@ -39,11 +39,74 @@ export async function POST(request) {
       });
     };
     
-    const liked = userQuery.liked ? `${userQuery.liked},${id}` : id.toString();
+    const liked = userQuery.liked ? `${userQuery.liked}, ${id}` : id.toString();
     
     await db.prepare('UPDATE users SET liked = ? WHERE username = ?').bind(liked, username).run();
     
     return new Response(JSON.stringify({ success: true, liked }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  };
+};
+
+export async function DELETE(request) {
+  const { env } = getRequestContext();
+  const authToken = env.CLIENT_AUTH;
+  const authResponse = bearerHeaders(request, authToken);
+
+  if (authResponse) return authResponse;
+
+  let requestBody;
+  try {
+    requestBody = JSON.parse(await request.text());
+  } catch {
+    return new Response(JSON.stringify({ error: 'Invalid JSON format' }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  };
+
+  const { id, username } = requestBody;
+
+  if (!id || !username) {
+    return new Response(JSON.stringify({ error: 'All fields are required.' }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  };
+
+  const db = env.D1;
+
+  try {
+    const userQuery = await db.prepare('SELECT liked FROM users WHERE username = ?').bind(username).first();
+    
+    if (!userQuery) {
+      return new Response(JSON.stringify({ error: 'User not found.' }), {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' }
+      });
+    };
+
+    const likedArray = userQuery.liked ? userQuery.liked.split(', ').filter(likedId => likedId !== id) : [];
+
+    if (likedArray.length === userQuery.liked.split(', ').length) {
+      return new Response(JSON.stringify({ error: 'ID not found in liked.' }), {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+    const updatedLiked = likedArray.join(', ');
+
+    await db.prepare('UPDATE users SET liked = ? WHERE username = ?').bind(updatedLiked, username).run();
+
+    return new Response(JSON.stringify({ success: true, liked: updatedLiked }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' }
     });
