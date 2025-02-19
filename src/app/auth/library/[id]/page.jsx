8a6f5d7395ef565228f5b430c78df0d06146be38
_niezle_ziolko <wgwcompany@duck.com>
2025-02-19 20:41:32 +0100

    update

--- src/app/auth/library/[id]/page.jsx
+++ src/app/auth/library/[id]/page.jsx
@@ -1,22 +1,39 @@
+import { getRequestContext } from '@cloudflare/next-on-pages';
+
 import { notFound } from 'next/navigation';
 
 import AudioPlayer from 'components/player';
 import BookPanel from 'components/panel/book-panel';
 
-import { getBookById } from 'lib/fetch-book';
-
 export default async function Page({ params }) {
   const { id } = await params;
-  const book = await getBookById(id);
+  const { env } = getRequestContext();
 
-  if (!book) {
+  try {
+    const response = await fetch(`https://cs50-final-project.niezleziolko.app/api/data/book?id=${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${env.NEXT_PUBLIC_CLIENT_AUTH}`
+      }
+    });
+
+    const book = await response.json();
+
+    if (!book || !id) {
+      notFound();
+    };
+
+    return (
+      <>
+        <BookPanel book={book} />
+        <AudioPlayer />
+      </>
+    );
+  } catch (error) {
+    console.error('Error:', error);
     notFound();
   };
-
-  return (
-    <div className='page'>
-      <BookPanel book={book} />
-      <AudioPlayer />
-    </div>
-  );
 };
+
+export const runtime = 'edge';
